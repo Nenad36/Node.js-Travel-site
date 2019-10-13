b9b0524f4c79e5cf2658087d617ff0eef223446b
Nenad36 <nenad.pesic32@gmail.com>
2019-10-13 16:17:48 +0200

    Travel Site

---
+++ server/config/database.js
@@ -0,0 +1,19 @@
+const Sequelize = require('sequelize');
+
+module.exports = new Sequelize('travelagency', 'root', '', {
+    host: '127.0.0.1',
+    port: '3306',
+    dialect: 'mysql',
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    },
+    define: {
+        timestamps: false
+    },
+    operatorsAliases: false
+
+
+});
